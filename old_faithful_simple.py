0402982d4f5307272b217fa16fa30b739dd11ed3
jakefrisbee <jakefrisbee@yahoo.com>
2014-02-04 14:02:14 -0700

    Adding more geysers and parameters

--- old_faithful_simple.py
+++ old_faithful_simple.py
@@ -23,30 +23,42 @@ url = "http://www.geysertimes.org/api/v2/entries_latest/2;4;5;7;13"
 content = urllib2.urlopen(url).read()
 myJSON = json.loads(content)
 
-#Old Faithful entry is the first in the entries list
-old_faithful = myJSON['entries'][0]
-
-window = 10
-
-# if Old Faithful is marked as a minor (i.e. a "short"), use shorter interval 
-if old_faithful['min'] == 1:
-    interval = 62 #minutes
-else:
-    interval = 95 #minutes
-
-# slight adjustment for ie times
-if old_faithful['ie'] == 1:
-    interval = interval - 2
-    window = window + 1
+#dictionary of prediciton parameters
+#NPS values as of January 2014
+params = {
+  'Old Faithful': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Daisy': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Castle': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Riverside': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Grand': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1}
+  }
 
 
-myTime = old_faithful['time']
+predict('Old Faithful',myJSON['entries'][0])
+predict('Daisy',myJSON['entries'][1])
+predict('Castle',myJSON['entries'][2])
+predict('Riverside',myJSON['entries'][3])
+predict('Grand',myJSON['entries'][4])
 
-prediction = int(old_faithful['time']) + (interval * 60)
-out_prediction = time.strftime('%Y-%m-%d %H:%M', time.gmtime(prediction))
 
+def predict(geyser,entry):
 
+  myTime = entry['time']
+  
+  if entry['min'] == 1 and params[geyser]['interval_min']:
+      interval = params[geyser]['interval_min'] #minutes
+  else:
+      interval = params[geyser]['interval_maj'] #minutes
 
-print "Old Faithful is predicted for %s +/- %s minutes" % (out_prediction, window)
-print "Send to GeyserTimes.org: {'prediction': %s, 'window': %s}" % (prediction  , window)
+  window = params[geyser]['window']
+  # slight adjustment for ie times
+  if entry['ie'] == 1:
+      interval = interval - params[geyser]['ie_interval_adjustment']
+      window = window + params[geyser]['ie_window_adjustment']
+
+  prediction = int(myTime) + (interval * 60)
+  out_prediction = time.strftime('%Y-%m-%d %H:%M', time.gmtime(prediction))
+
+  print "%s Geyser is predicted for %s +/- %s minutes" % (geyser, out_prediction, window)
+  print "Send to GeyserTimes.org: {'GeyserID': %s, 'prediction': %s, 'window': %s}" % (entry['geyserID'], prediction  , window)
 
