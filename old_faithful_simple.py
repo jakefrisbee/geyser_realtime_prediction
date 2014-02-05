5ae7d68c02ca01111dd312b4b756bff9800ddaf0
jakefrisbee <jakefrisbee@yahoo.com>
2014-02-04 17:40:59 -0700

    Update old_faithful_simple.py

--- old_faithful_simple.py
+++ old_faithful_simple.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 02 19:58:42 2014
-
-@author: Jake
-"""
 
 import urllib2 #for API calls to GeyserTimes.org
 import json #API returns JSON
@@ -27,12 +22,38 @@ myJSON = json.loads(content)
 #NPS values as of January 2014
 #Castle needs its own method
 params = {
-  'Old Faithful': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
-  'Daisy': {'interval_min': 2.75 * 60, 'interval_max': 2.75 * 60, 'window' : 15, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
-  #'Castle': {'interval_min': 62, 'interval_max': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
-  'Riverside': {'interval_min': 6 * 60, 'interval_max': 6 * 60, 'window' : 30, 'ie_interval_adjustment': 15, 'ie_window_adj': 10},
-  'Grand': {'interval_min': 7.5 * 60, 'interval_max': 7.5 * 60, 'window' : 1.5 * 60, 'ie_interval_adjustment': 5, 'ie_window_adj': 10}
+  'Old Faithful': {'interval_min': 62, 'interval_maj': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Daisy': {'interval_min': 2.75 * 60, 'interval_maj': 2.75 * 60, 'window' : 15, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  #'Castle': {'interval_min': 62, 'interval_maj': 95, 'window' : 10, 'ie_interval_adjustment': 2, 'ie_window_adj': 1},
+  'Riverside': {'interval_min': 6 * 60, 'interval_maj': 6 * 60, 'window' : 30, 'ie_interval_adjustment': 15, 'ie_window_adj': 10},
+  'Grand': {'interval_min': 7.5 * 60, 'interval_maj': 7.5 * 60, 'window' : 1.5 * 60, 'ie_interval_adjustment': 5, 'ie_window_adj': 10}
   }
+
+tz_offset = -7
+
+
+
+def predict(geyser,entry):
+
+    myTime = int(entry['time'])
+    
+    if entry['min'] == 1 and params[geyser]['interval_min']:
+        interval = params[geyser]['interval_min'] #minutes
+    else:
+        interval = params[geyser]['interval_maj'] #minutes
+
+    window = params[geyser]['window']
+    
+    # slight adjustment for ie times
+    if entry['ie'] == 1:
+        interval = interval - params[geyser]['ie_interval_adjustment']
+        window = window + params[geyser]['ie_window_adjustment']
+
+    prediction = int(myTime) + (interval * 60)
+    out_prediction = time.strftime('%Y-%m-%d %H:%M', time.gmtime(prediction))
+
+    print "%s Geyser is predicted for %s +/- %s minutes" % (geyser, out_prediction, window)
+    print "Send to GeyserTimes.org: {'GeyserID': %s, 'prediction': %s, 'window': %s}" % (entry['geyserID'], prediction + tz_offset , window)
 
 
 predict('Old Faithful',myJSON['entries'][0])
@@ -40,26 +61,3 @@ predict('Daisy',myJSON['entries'][1])
 #predict('Castle',myJSON['entries'][2])
 predict('Riverside',myJSON['entries'][3])
 predict('Grand',myJSON['entries'][4])
-
-
-def predict(geyser,entry):
-
-  myTime = int(entry['time'])
-  
-  if entry['min'] == 1 and params[geyser]['interval_min']:
-      interval = params[geyser]['interval_min'] #minutes
-  else:
-      interval = params[geyser]['interval_maj'] #minutes
-
-  window = params[geyser]['window']
-  # slight adjustment for ie times
-  if entry['ie'] == 1:
-      interval = interval - params[geyser]['ie_interval_adjustment']
-      window = window + params[geyser]['ie_window_adjustment']
-
-  prediction = int(myTime) + (interval * 60)
-  out_prediction = time.strftime('%Y-%m-%d %H:%M', time.gmtime(prediction))
-
-  print "%s Geyser is predicted for %s +/- %s minutes" % (geyser, out_prediction, window)
-  print "Send to GeyserTimes.org: {'GeyserID': %s, 'prediction': %s, 'window': %s}" % (entry['geyserID'], prediction  , window)
-
